85d8c2932475c3e883ece1c32c6ed3dc02a787aa
Nick Vrdoljak <admin@nicholasvrdoljak.com>
2018-05-02 15:44:49 -0700

    voted event

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,6 +7,8 @@ contract Election {
         string name;
         uint voteCount;
     }
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
 
     // Store Candidates
     // Fetch a Candidate
@@ -15,7 +17,12 @@ contract Election {
 
     // Store Candidates Count
     uint public candidatesCount;
-    
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
     //Constructor
     constructor () public {
         addCandidate("Candidate 1");
@@ -26,5 +33,22 @@ contract Election {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
         //              key                  value 
+    }
+
+    function vote (uint _candidateId) public {
+        // user hasn't voted before
+        require(!voters[msg.sender]);
+
+        // voting for a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted 
+        voters[msg.sender] = true;
+
+        // update candidate vote count
+        candidates[_candidateId].voteCount++;
+
+        // trigger event 
+        emit votedEvent(_candidateId);
     }
 }
